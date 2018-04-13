95720e130faf7e4cab7e5dd6c0524c760ac53f85
chunkitmax <chunkitmax1125@gmail.com>
2018-04-14 01:36:37 +0800

    Update for general use

--- convnet.py
+++ convnet.py
@@ -10,8 +10,8 @@ import torch as T
 
 class ConvNet(T.nn.Module):
   def __init__(self, embedding_len, num_vocab, num_class, doc_len,
-               lr=.01, momentum=.9, ngrams=1, drop_rate=.5,
-               num_filter=3, stride=1, use_bn=False, use_cuda=False):
+               lr=.01, momentum=.9, kernel_size=[1], num_filter=[3],
+               drop_rate=.5, stride=1, use_bn=False, use_cuda=False):
     super(ConvNet, self).__init__()
     # member declaration
     self.embedding_len = embedding_len
@@ -20,9 +20,9 @@ class ConvNet(T.nn.Module):
     self.doc_len = doc_len
     self.lr = lr
     self.momentum = momentum
-    self.ngrams = ngrams
-    self.drop_rate = drop_rate
+    self.kernel_size = kernel_size
     self.num_filter = num_filter
+    self.drop_rate = drop_rate
     self.stride = stride
     self.use_bn = use_bn
     self.use_cuda = use_cuda
@@ -38,28 +38,30 @@ class ConvNet(T.nn.Module):
 
     # Embedding
     self.Embedding = T.nn.Embedding(self.num_vocab, self.embedding_len)
-    self.CNN_ngrams = []
+    self.CNN = []
     self.MaxPool = []
     self.LeakyReLU = []
     self.BatchNorm = []
     self.DropOut = []
+    flatten_size = 0
     # Convolution + MaxPool
-    for i in range(self.ngrams):
-      tmp_CNN = T.nn.Conv2d(self.stride, self.num_filter, (i+1, self.embedding_len), bias=False)
+    for i in range(len(self.kernel_size)):
+      tmp_CNN = T.nn.Conv2d(self.stride, self.num_filter[i], (self.kernel_size[i], self.embedding_len), bias=False)
       tmp_CNN.apply(init_weight)
-      self.CNN_ngrams.append(tmp_CNN)
+      self.CNN.append(tmp_CNN)
       self.LeakyReLU.append(T.nn.LeakyReLU())
       self.MaxPool.append(T.nn.MaxPool2d((self.doc_len-i, i+1)))
       self.DropOut.append(T.nn.Dropout2d(self.drop_rate))
       if self.use_bn:
-        self.BatchNorm.append(T.nn.BatchNorm2d(self.num_filter))
+        self.BatchNorm.append(T.nn.BatchNorm2d(self.num_filter[i]))
+      flatten_size += self.kernel_size[i]*self.num_filter[i]
     # Dense
-    self.Fc = T.nn.Linear(self.ngrams*self.num_filter, self.num_class)
+    self.Fc = T.nn.Linear(flatten_size, self.num_class)
     # Loss
     self.Loss_fn = T.nn.CrossEntropyLoss()
     # GPU support
     if self.use_cuda:
-      [CNN.cuda() for CNN in self.CNN_ngrams]
+      [CNN.cuda() for CNN in self.CNN]
       [MP.cuda() for MP in self.MaxPool]
       [BN.cuda() for BN in self.BatchNorm]
       self.cuda()
@@ -73,14 +75,14 @@ class ConvNet(T.nn.Module):
     batch_size = inputs.size()[0]
     embeddings = self.Embedding(inputs)
     embeddings.unsqueeze_(1)
-    ngram_output = []
-    for i in range(self.ngrams):
-      tmp_output = self.MaxPool[i](self.LeakyReLU[i](self.CNN_ngrams[i](embeddings)))
+    CNN_output = []
+    for i in range(len(self.kernel_size)):
+      tmp_output = self.MaxPool[i](self.LeakyReLU[i](self.CNN[i](embeddings)))
       tmp_output = self.DropOut[i](tmp_output)
       if self.use_bn:
         tmp_output = self.BatchNorm[i](tmp_output)
-      ngram_output.append(tmp_output)
-    concat = T.cat(ngram_output, 1).view([batch_size, -1])
+      CNN_output.append(tmp_output)
+    concat = T.cat(CNN_output, 1).view([batch_size, -1])
     output = self.Fc(concat)
     return output, T.max(output, dim=1)
   def get_loss_fn(self):
