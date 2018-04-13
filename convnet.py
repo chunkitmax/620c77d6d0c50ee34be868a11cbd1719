c9c9537c7ff83eda1d5814f88aec207d6a4be903
chunkitmax <chunkitmax1125@gmail.com>
2018-04-14 01:19:51 +0800

    Update

--- convnet.py
+++ convnet.py
@@ -49,10 +49,10 @@ class ConvNet(T.nn.Module):
       tmp_CNN.apply(init_weight)
       self.CNN_ngrams.append(tmp_CNN)
       self.LeakyReLU.append(T.nn.LeakyReLU())
-      self.MaxPool.append(T.nn.MaxPool2d((self.doc_len-i, i+1)))
-      self.DropOut.append(T.nn.Dropout2d(self.drop_rate))
+      self.MaxPool.append(T.nn.MaxPool2d((self.doc_len-i, 1)))
       if self.use_bn:
         self.BatchNorm.append(T.nn.BatchNorm2d(self.num_filter))
+      self.DropOut.append(T.nn.Dropout2d(self.drop_rate))
     # Dense
     self.Fc = T.nn.Linear(self.ngrams*self.num_filter, self.num_class)
     # Loss
@@ -76,13 +76,14 @@ class ConvNet(T.nn.Module):
     ngram_output = []
     for i in range(self.ngrams):
       tmp_output = self.MaxPool[i](self.LeakyReLU[i](self.CNN_ngrams[i](embeddings)))
-      tmp_output = self.DropOut[i](tmp_output)
       if self.use_bn:
         tmp_output = self.BatchNorm[i](tmp_output)
+      tmp_output = self.DropOut[i](tmp_output)
       ngram_output.append(tmp_output)
     concat = T.cat(ngram_output, 1).view([batch_size, -1])
     output = self.Fc(concat)
-    return output, T.max(output, dim=1)
+    _, max_indice = T.max(output, dim=1)
+    return output, max_indice
   def get_loss_fn(self):
     return self.Loss_fn
   def get_optimizer(self):
